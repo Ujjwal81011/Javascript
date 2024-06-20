849a773e09d1fa0eeffd4bf3999f8701ec8671e8
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-06-20 16:49:26 +0530

    Function

--- 02 basics/05_function.js
+++ 02 basics/05_function.js
@@ -1,3 +1,35 @@
-console.log("hello Chai aur Code");
-console.log("hello github");
-console.log("new git hub");
+function myname(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// myname()
+
+// function sum(num1,num2){
+//     console.log(num1+num2);
+// }
+// const result=sum(5,8)
+// console.log("Result: ",result);
+
+function sum(num1,num2){
+    return num1+num2
+    console.log("Hitesh"); // function will not return any console value ahter return 
+}
+const result=sum(5,8)
+console.log("Result: ",result);
+
+
+function usermessage(username = "Sam"){
+    if(username === undefined) // or if(!username)
+        {
+            console.log("please enter a username");
+            return
+        }
+    return `${username} just logged in`
+}
+// console.log(usermessage("Ujjwal"))
+console.log(usermessage());
