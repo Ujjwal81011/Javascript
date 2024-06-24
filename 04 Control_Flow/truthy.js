ceccbc5909f6ec5e0c841adf60c2004b5f6ed99a
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-06-24 19:09:14 +0530

    truthy falsy

---
+++ 04 Control_Flow/truthy.js
@@ -0,0 +1,33 @@
+const useremial = []
+
+if(useremial){
+    console.log("Got user emeil")
+}
+else{
+    console.log("Don't have user email");
+}
+
+// falsy values => ( false, 0, BigInt 0n, "", null, undefined, NaN )
+// truthy values => ( "0", 1, "false", " ", [], (), function(){} )
+
+if (useremial.length === 0) {
+    console.log("Array is empty");
+}
+
+const emptyobject = {}
+if (Object.keys(emptyobject).length === 0) { // convert the object into array
+    console.log("object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefind
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15  
+console.log(val1);
+
+// turniary operator
+// condition ? true : false
+const iceteaprice = 100
+iceteaprice <= 80 ? console.log("less than 80") : console.log("more than 80")
