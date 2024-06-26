f916d8a9878b36ed531fdbc5de073ec1795fa683
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-06-26 15:40:31 +0530

    do while

---
+++ 02_while.js
@@ -0,0 +1,28 @@
+let index = 0
+
+// while (index <= 10) {
+//     console.log(`value of the index is: ${index}`)
+//     index = index + 2
+// }
+
+let myarray = ["flash", "batman", "superman"]
+let arr = 0
+while (arr < myarray.length) {
+    console.log(`value is ${myarray[arr]}`)
+    arr = arr + 1
+}
+
+// for (let index = 0; index < myarray.length; index++) { //practice of for loop
+//     console.log(`value is ${myarray[index]}`)
+// }
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(index + "*" + index + "*" + index + "=" + index*index*index);
+// }
+
+// DO while loop
+let score = 11
+do {
+    console.log(`score is: ${score}`);
+    score++
+} while (score <= 10);
